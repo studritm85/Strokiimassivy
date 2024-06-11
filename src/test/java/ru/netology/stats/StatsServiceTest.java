f2a888ccfd484f7dbbe65c787776d21f9474652e
Ekaterina <studritm@gmail.com>
2024-06-11 15:41:33 +0400

    initial commit

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -55,8 +55,10 @@ class StatsServiceTest {
     void shouldCountMonthsAboveAverage() {
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 10, 21};
-        int expected = 5;
+        int expected = 7; // Исправленное ожидаемое значение
         int actual = service.countMonthsAboveAverage(sales);
         assertEquals(expected, actual);
     }
+
+
 }
